4ec7cce32c446bebd2f1893c1ba112ffb8d8d55b
ThiagoSezerio123 <sezerio.thiago@escola.pr.gov.br>
2026-09-09 09:44:30 -0300

    9/9

---
+++ main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorAll(".Botao");
+
+for (let i=0; i<botoes. lenght; i++) {
+    botoes[i].onclick = function {
+        botoes[i].classlit.add("ativo") 
+    }
+} 
+
